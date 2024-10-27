5f3dfc2a87af5f1df3cd88f27ac113cb3edf3415
sora-yuka <feelingjeez@gmail.com>
2024-10-27 23:05:09 +0600

    added static root. deleted migrations

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 import dj_config_url
 from pathlib import Path
 from datetime import timedelta
@@ -141,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
